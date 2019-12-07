952b2990b894c6d9fc7385bfd79070a6ab7fa0da
Tin Tran <tin.tran0519@gmail.com>
2019-12-07 12:38:38 -0800

    refactored users controller show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def show
-    render json: { user: current_user }
+    render json: { user: @user }
+  end
+
+  private
+
+  def set_user
+    @user = {
+      username: current_user.username,
+      vehicles: current_user.vehicles
+    }
   end
 end
